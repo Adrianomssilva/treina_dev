97682152e486b1a56d43528e0a6b154562f8c19f
devadrsilva <adriano.mssilva1@gmail.com>
2023-09-18 14:31:46 -0300

    menu add

--- cookbook.rb
+++ cookbook.rb
@@ -1,17 +1,29 @@
 puts 'Bem vindo ao Cookbook sua rede social de receitas!'
 
 receitas = []
-while (true)
-  
 
-puts 'Digite o nome da receita: '
-receita = gets.chomp
-receitas << receita
-puts
-puts "Receita #{receita} cadastrada com sucesso!!"
-puts
-puts "========= Receitas cadastradas ========="
-  receitas.each  do |r|
-    puts r
+puts "[1] Cadastrar uma receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
+
+print "Escolha uma opção: "
+opcao = gets.to_i
+
+while (opcao != 3)
+  if opcao == 1
+    puts 'Digite o nome da receita: '
+    nome = gets.chomp
+    receitas << nome
+    puts
+    puts "Receita #{nome} cadastrada com sucesso!!"
+    puts
+  elsif opcao == 2
+    puts "========= Receitas cadastradas ========="
+      receitas.each  do |receita|
+        puts receita
+      end
+  else
+    puts "Digite uma opção válida!"
   end
 end
+puts "Obrigado por usar o Cookbook!!"
